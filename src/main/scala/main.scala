690a07d3032ea3433dc9ff9e448ff13f3c531359
MielHostens <miel.hostens@cornell.edu>
2025-03-25 15:54:54 -0400

    first commit

---
+++ src/main/scala/main.scala
@@ -0,0 +1,11 @@
+object Main {
+
+  /** Represents the main class.
+   *
+   * Prints 'Hello world'
+   */
+
+  def main(args: Array[String]): Unit = {
+    println("Successful Dairy Cow Domain Model Build!!!")
+  }
+}
